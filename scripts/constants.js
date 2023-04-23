2330060e7c1887068352c189a7431f60e4971937
Irina <i.sus2011@yandex.ru>
2023-04-23 15:03:34 +0300

    refactor: создание отдельных классов по ооп

--- scripts/constants.js
+++
@@ -1,9 +0,0 @@
-//  для валидации форм
-export const config = {
-  formSelector: ".popup__form",
-  inputSelector: ".popup__input",
-  submitButtonSelector: ".popup__button",
-  inactiveButtonClass: "popup__button_disabled",
-  inputErrorClass: "popup__input_type_error",
-  errorClass: "popup__error_visible",
-};
