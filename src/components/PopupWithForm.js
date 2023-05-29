478fc3709c82e90142cbfc2f201964c61f21cf01
Irina <i.sus2011@yandex.ru>
2023-05-29 12:26:47 +0300

    fix:Исправить закрытие попапа после удачного ответа сервера

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -6,25 +6,30 @@ export default class PopupWithForm extends Popup {
     this._handleSubmitForm = handleSubmitForm;
     this._submitForm = this._submitForm.bind(this);
     this._form = this._popup.querySelector(".popup__form");
+    this._inputs = Array.from(this._form.querySelectorAll(".popup__input"));
     this._submitButton = this._form.querySelector(".popup__button");
-  }
-  renderLoading(isLoading) {
-    if (isLoading) {
-      this._submitButton.textContent = "Сохранение...";
-    } else {
-      this._submitButton.textContent = "Сохранить";
-    }
-  }
-  _submitForm(evt) {
-    evt.preventDefault();
-    this._handleSubmitForm(this._getInputValues(), this._submitButton);
-    this.close();
+    this._initialText = this._submitButton.textContent;
   }
 
+  _submitForm = (evt) => {
+    evt.preventDefault();
+    const initialButtonText = this._submitButton.textContent;
+    this._submitButton.textContent = "Сохранение...";
+    this._handleSubmitForm(this._getInputValues())
+      .then(() => {
+        this._submitButton.textContent = initialButtonText;
+        this.close();
+      })
+      .catch((error) => {
+        console.error(error);
+        this._submitButton.textContent = initialButtonText;
+      });
+  };
+
+  // получаем значения полей формы
   _getInputValues() {
-    const inputs = Array.from(this._form.querySelectorAll(".popup__input"));
     const values = {};
-    inputs.forEach((input) => {
+    this._inputs.forEach((input) => {
       values[input.name] = input.value;
     });
     return values;
@@ -32,11 +37,17 @@ export default class PopupWithForm extends Popup {
 
   setEventListeners() {
     super.setEventListeners();
-
     this._form.addEventListener("submit", this._submitForm);
   }
 
-  // Закрытие попапа и сброс инпутов
+  // устаноавливаем значения полей формы
+  setInputValues(data) {
+    this._inputs.forEach((input) => {
+      input.value = data[input.name];
+    });
+  }
+
+  // закрываем попап и сбросываем инпуты
   close() {
     super.close();
     this._form.reset();
